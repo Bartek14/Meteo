15182a5bc008d65a52882fdc5fe15ddcd2141089
PiotrMoscicki <PiOtR3830>
2017-04-25 21:22:15 +0200

    Init fixes

--- Master/Master/Master.ino
+++ Master/Master/Master.ino
@@ -1,15 +1,8 @@
-/*
- Name:		Master.ino
- Created:	3/3/2017 3:41:43 PM
- Author:	mpiot
-*/
 
-// the setup function runs once when you press reset or power the board
 void setup() {
 
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
   
 }
---
+++ Master/Master/src/DataCollectingSystem.cpp
@@ -0,0 +1,3 @@
+#include "DataCollectingSystem.hpp"
+
+using namespace Meteo;
